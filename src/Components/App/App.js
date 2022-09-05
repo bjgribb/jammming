5231922d41e4ed61f11d78fe429cf14cce4c5111
Brian Gribbon <GRIBBOBJ@legal.regn.net>
2022-09-05 18:33:08 -0400

    resolved map error...

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,29 @@ import SearchResults from "../SearchResults/SearchResults";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: "Meteorite",
+          artist: "Anna of the North, Gus Dapperton",
+          album: "Single",
+          id: "1",
+        },
+      ],
+      playlistName: "",
+      playlistTracks: [
+        {
+          name: "Meteorite Again",
+          artist: "Anna of the North, Gus Dapperton",
+          album: "Single",
+          id: "2",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div>
@@ -14,8 +37,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist tracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
